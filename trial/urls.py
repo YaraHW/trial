d0945ec4080608fe7e6ad92a5cddfa070bc9e0eb
Kirill Rumynin <kirill.rumynin@lamoda.ru>
2021-05-04 12:10:38 +0300

    redirect to index page for authinticated user for django way 1.11

--- trial/urls.py
+++ trial/urls.py
@@ -2,14 +2,14 @@ from django.urls import path, include
 from django.contrib import admin
 from . import views
 from .views import SignUpView
-from .views import custom_login
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('accounts/signup/', SignUpView.as_view(), name='signup'),
 
+    path('accounts/login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
 
-    path('accounts/login/', custom_login, name='login'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
--- trial/views.py
+++ trial/views.py
@@ -2,23 +2,12 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-from django.contrib.auth.views import auth_login
-from django.shortcuts import redirect
 
 
 def index(request):
-
-
     return render(request, './base_generic.html' )
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
-
-
-def custom_login(request):
-    if request.user.is_authenticated:
-        return redirect('index')
-    else:
-        return auth_login(request)

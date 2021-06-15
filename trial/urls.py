d49b8121f0e61ef65256021974e57d434773fff2
YaraHW <mister.chepurnoy@mail.ru>
2021-06-15 20:21:20 +1100

    button playgame with authorization check added

--- trial/urls.py
+++ trial/urls.py
@@ -8,9 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('accounts/signup/', SignUpView.as_view(), name='signup'),
-
     path('accounts/login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
-
     path('accounts/', include('django.contrib.auth.urls')),
     path('play_game/', views.playgame, name = "play_game")
 ]
--- trial/views.py
+++ trial/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth.decorators import login_required  # стянули декоратор чтоб не было возможности открыть страницы без авторизации
+
 
 
 def index(request):
     return render(request, './base_generic.html' )
 
+@login_required
 def playgame(request):
     return render(request, './play_game.html')
 
